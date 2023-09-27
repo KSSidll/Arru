0a88bc40b1965c56506603c435d1dc73472f6e5e
KSSidll <kssidll@users.noreply.github.com>
2023-09-27 20:50:36 +0200

    Remove accidental old implementation comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,12 +60,6 @@ android {
 dependencies {
 
     // AndroidX
-    //    implementation("androidx.activity:activity-compose:1.7.2")
-    //    implementation("androidx.core:core-splashscreen:1.0.1")
-    //    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
-    //    implementation("androidx.datastore:datastore-preferences-android:1.1.0-alpha05")
-    //    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    //    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
     implementation(libs.activity.compose)
     implementation(libs.core.splashscreen)
     implementation(libs.appcompat)
@@ -74,20 +68,10 @@ dependencies {
     implementation(libs.accompanist.systemuicontroller)
 
     // Navigation
-    //    implementation("dev.olshevski.navigation:reimagined:1.5.0")
-    //    implementation("dev.olshevski.navigation:reimagined-hilt:1.5.0")
     implementation(libs.reimagined)
     implementation(libs.reimagined.hilt)
 
     // Compose
-    //    implementation("androidx.compose.ui:ui:1.6.0-alpha06")
-    //    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0-alpha06")
-    //    debugImplementation "androidx.compose.ui:ui-tooling:1.6.0-alpha06"
-    //    debugImplementation "androidx.compose.ui:ui-test-manifest:1.6.0-alpha06"
-    //    implementation("androidx.compose.material3:material3:1.2.0-alpha08")
-    //    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha08")
-    //    implementation("androidx.compose.material:material-icons-extended:1.6.0-alpha06")
-    //    implementation("androidx.compose.material:material:1.6.0-alpha06")
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
     debugImplementation(libs.compose.ui.tooling)
@@ -98,23 +82,15 @@ dependencies {
     implementation(libs.material)
 
     // DI
-    //    implementation("com.google.dagger:hilt-android:2.48")
-    //    kapt("com.google.dagger:hilt-compiler:2.48")
     implementation(libs.hilt)
     kapt(libs.hilt.compiler)
 
     // Room
-    //    implementation("androidx.room:room-ktx:2.6.0-rc01")
-    //    ksp("androidx.room:room-compiler:2.6.0-rc01")
-    //    ksp("androidx.room:room-ktx:2.6.0-rc01")
     implementation(libs.room.ktx)
     ksp(libs.room.ktx)
     ksp(libs.room.compiler)
 
-
-    //    implementation("me.xdrop:fuzzywuzzy:1.4.0")
-    //    implementation("com.patrykandpatrick.vico:compose:1.12.0")
-    //    implementation("com.patrykandpatrick.vico:compose-m3:1.12.0")
+    // Other
     implementation(libs.fuzzywuzzy)
     implementation(libs.vico.compose)
     implementation(libs.vico.compose.m3)
