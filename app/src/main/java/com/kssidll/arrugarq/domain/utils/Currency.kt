77a98e1b2ae702743913de3ccd98c512941544a1
KSSidll <kssidll@users.noreply.github.com>
2023-09-30 21:05:21 +0200

    Implement currency formatting helper

---
+++ app/src/main/java/com/kssidll/arrugarq/domain/utils/Currency.kt
@@ -0,0 +1,20 @@
+package com.kssidll.arrugarq.domain.utils
+
+import android.icu.util.Currency
+import java.text.*
+import java.util.*
+
+fun Long.formatToCurrency(
+    locale: Locale = Locale.getDefault(),
+): String {
+    val dropAmount = Currency.getInstance(locale).defaultFractionDigits + 1
+    return NumberFormat.getCurrencyInstance(locale).format(this).dropLast(dropAmount)
+}
+
+fun Float.formatToCurrency(
+    locale: Locale = Locale.getDefault(),
+    dropDecimal: Boolean = false,
+): String {
+    val dropAmount = if (dropDecimal) Currency.getInstance(Locale.getDefault()).defaultFractionDigits + 1 else 0
+    return NumberFormat.getCurrencyInstance(locale).format(this).dropLast(dropAmount)
+}
