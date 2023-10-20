e562d9daf2dde1630a0229a01f46fba928e217ad
KSSidll <kssidll@users.noreply.github.com>
2023-10-20 20:05:49 +0200

    Chore: Modularise AddProductCategoryScreen component implementation

--- app/src/main/java/com/kssidll/arrugarq/ui/screen/addproductcategory/AddProductCategoryRoute.kt
+++ app/src/main/java/com/kssidll/arrugarq/ui/screen/addproductcategory/AddProductCategoryRoute.kt
@@ -1,6 +1,7 @@
 package com.kssidll.arrugarq.ui.screen.addproductcategory
 
 import androidx.compose.runtime.*
+import com.kssidll.arrugarq.ui.screen.shared.*
 import dev.olshevski.navigation.reimagined.hilt.*
 import kotlinx.coroutines.*
 
@@ -11,10 +12,10 @@ fun AddProductCategoryRoute(
     val scope = rememberCoroutineScope()
     val addProductCategoryViewModel: AddProductCategoryViewModel = hiltViewModel()
 
-    AddProductCategoryScreen(
+    EditProductCategoryScreen(
         onBack = onBack,
-        state = addProductCategoryViewModel.addProductCategoryScreenState,
-        onCategoryAdd = {
+        state = addProductCategoryViewModel.screenState,
+        onSubmit = {
             scope.launch {
                 val result = addProductCategoryViewModel.addCategory()
                 if (result != null) onBack()
--- app/src/main/java/com/kssidll/arrugarq/ui/screen/addproductcategory/AddProductCategoryViewModel.kt
+++ app/src/main/java/com/kssidll/arrugarq/ui/screen/addproductcategory/AddProductCategoryViewModel.kt
@@ -1,62 +1,25 @@
 package com.kssidll.arrugarq.ui.screen.addproductcategory
 
-import androidx.compose.runtime.*
 import androidx.lifecycle.*
-import com.kssidll.arrugarq.data.data.*
 import com.kssidll.arrugarq.domain.repository.*
+import com.kssidll.arrugarq.ui.screen.shared.*
 import dagger.hilt.android.lifecycle.*
 import kotlinx.coroutines.*
 import javax.inject.*
-
-internal data class AddProductCategoryScreenState(
-    val attemptedToSubmit: MutableState<Boolean> = mutableStateOf(false),
-
-    val name: MutableState<String> = mutableStateOf(String()),
-    val nameError: MutableState<Boolean> = mutableStateOf(false),
-)
-
-/**
- * Validates name field and updates its error flag
- * @return true if field is of correct value, false otherwise
- */
-internal fun AddProductCategoryScreenState.validateName(): Boolean {
-    return !(name.value.isBlank()).also { nameError.value = it }
-}
-
-/**
- * Validates state fields and updates state flags
- * @return true if all fields are of correct value, false otherwise
- */
-internal fun AddProductCategoryScreenState.validate(): Boolean {
-    return validateName()
-}
-
-/**
- * performs data validation and tries to extract embedded data
- * @return Null if validation sets error flags, extracted data otherwise
- */
-internal fun AddProductCategoryScreenState.extractCategoryOrNull(): ProductCategory? {
-    if (!validate()) return null
-
-    return ProductCategory(
-        name = name.value.trim(),
-    )
-}
 
 @HiltViewModel
 class AddProductCategoryViewModel @Inject constructor(
     private val categoryRepository: IProductCategoryRepository,
 ): ViewModel() {
-    internal val addProductCategoryScreenState: AddProductCategoryScreenState =
-        AddProductCategoryScreenState()
+    internal val screenState: EditProductCategoryScreenState = EditProductCategoryScreenState()
 
     /**
      * Tries to add a product category to the repository
      * @return Id of newly inserted row, null if operation failed
      */
     suspend fun addCategory(): Long? = viewModelScope.async {
-        addProductCategoryScreenState.attemptedToSubmit.value = true
-        val category = addProductCategoryScreenState.extractCategoryOrNull() ?: return@async null
+        screenState.attemptedToSubmit.value = true
+        val category = screenState.extractCategoryOrNull() ?: return@async null
 
         return@async categoryRepository.insert(category)
     }
