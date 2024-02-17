b14c16b6c4f6adea826e05504adb4ead422fd264
KSSidll <kssidll@users.noreply.github.com>
2024-02-17 01:50:22 +0100

    Chore: Bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.kssidll.arrugarq"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
