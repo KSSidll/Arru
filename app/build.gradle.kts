ff44114d7b777ab2b9da2f3de6d81592bfb8e606
KSSidll <kssidll@users.noreply.github.com>
2024-03-21 00:01:41 +0100

    Chore: Update 2.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.kssidll.arru"
         minSdk = 21
         targetSdk = 34
-        versionCode = 32
-        versionName = "2.4.3"
+        versionCode = 33
+        versionName = "2.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
