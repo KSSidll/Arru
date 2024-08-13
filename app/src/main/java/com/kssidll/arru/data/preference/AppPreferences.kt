14e877cd42e2716d4c1e974e38b4004bb496e80e
Szymon Kolasa <kolasasidll@gmail.com>
2024-08-13 17:55:24 +0200

    Feat: Adaptive transaction date logic

--- app/src/main/java/com/kssidll/arru/data/preference/AppPreferences.kt
+++ app/src/main/java/com/kssidll/arru/data/preference/AppPreferences.kt
@@ -1,8 +1,11 @@
 package com.kssidll.arru.data.preference
 
-import android.content.*
-import androidx.datastore.preferences.core.*
-import com.kssidll.arru.di.module.*
+import android.content.Context
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.kssidll.arru.di.module.dataStore
 
 /**
  * Data associated with datastore preferences
@@ -15,7 +18,7 @@ data object AppPreferences {
      */
     data object Database {
         /**
-         * Data associated with database location datastore preferonce
+         * Data associated with database location datastore preference
          */
         data object Location {
             /**
@@ -39,6 +42,44 @@ data object AppPreferences {
             const val DEFAULT: String = EXTERNAL
         }
     }
+
+    /**
+     * Data associated with transaction datastore preferences
+     */
+    data object Transaction {
+        /**
+         * Data associated with transaction date datastore preference
+         */
+        data object Date {
+            /**
+             * Key for transaction date preference key-value pair
+             */
+            val key: Preferences.Key<Int> = intPreferencesKey("transactiondate")
+
+            /**
+             * Value for default date
+             */
+            val DEFAULT = Values.CURRENT
+
+            enum class Values {
+                /**
+                 * Value for current date
+                 */
+                CURRENT,
+
+                /**
+                 * Value for last transaction date
+                 */
+                LAST,
+
+                ;
+                companion object {
+                    private val idMap = Values.entries.associateBy { it.ordinal }
+                    fun getByOrdinal(ordinal: Int) = idMap[ordinal]
+                }
+            }
+        }
+    }
 }
 
 /**
@@ -53,4 +94,37 @@ suspend fun Preferences.setNullToDefault(context: Context) {
             it[AppPreferences.Database.Location.key] = AppPreferences.Database.Location.DEFAULT
         }
     }
+
+    if (this[AppPreferences.Transaction.Date.key] == null) {
+        context.dataStore.edit {
+            it[AppPreferences.Transaction.Date.key] = AppPreferences.Transaction.Date.DEFAULT.ordinal
+        }
+    }
+}
+
+/**
+ * Resets the preferences that should be reset on startup
+ * @param context App context
+ */
+suspend fun AppPreferences.setResettableToDefault(context: Context) {
+    setTransactionDate(context, AppPreferences.Transaction.Date.DEFAULT)
+}
+
+/**
+ * Sets the transaction date preference to a new value
+ * @param context App context
+ * @param newDatePreference Value to set the date preference to
+ */
+suspend fun AppPreferences.setTransactionDate(context: Context, newDatePreference: AppPreferences.Transaction.Date.Values) {
+    context.dataStore.edit {
+        it[AppPreferences.Transaction.Date.key] = newDatePreference.ordinal
+    }
+}
+
+/**
+ * Returns the transaction date preference value
+ * @return The transaction date preference value
+ */
+fun Preferences.getTransactionDate(): AppPreferences.Transaction.Date.Values {
+    return this[AppPreferences.Transaction.Date.key]?.let { AppPreferences.Transaction.Date.Values.getByOrdinal(it) } ?: AppPreferences.Transaction.Date.DEFAULT
 }
