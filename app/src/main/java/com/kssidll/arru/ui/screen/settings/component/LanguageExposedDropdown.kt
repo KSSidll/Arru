6585e0ac0344f6f50071784d74b647128226c169
Szymon Kolasa <kolasasidll@gmail.com>
2024-09-09 22:49:42 +0200

    Chore: update dependencies

--- app/src/main/java/com/kssidll/arru/ui/screen/settings/component/LanguageExposedDropdown.kt
+++ app/src/main/java/com/kssidll/arru/ui/screen/settings/component/LanguageExposedDropdown.kt
@@ -1,19 +1,32 @@
 package com.kssidll.arru.ui.screen.settings.component
 
 
-import androidx.appcompat.app.*
-import androidx.compose.foundation.*
-import androidx.compose.foundation.shape.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.compose.ui.res.*
-import androidx.compose.ui.tooling.preview.*
-import androidx.compose.ui.unit.*
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import com.kssidll.arru.R
-import com.kssidll.arru.domain.*
-import com.kssidll.arru.ui.theme.*
-import java.util.*
+import com.kssidll.arru.domain.AppLocale
+import com.kssidll.arru.ui.theme.ArrugarqTheme
+import com.kssidll.arru.ui.theme.Typography
+import java.util.Locale
 
 /**
  * Exposed dropdown to select locale
@@ -59,7 +72,9 @@ fun LanguageExposedDropdown(
                 unfocusedIndicatorColor = MaterialTheme.colorScheme.background,
                 focusedIndicatorColor = MaterialTheme.colorScheme.background,
             ),
-            modifier = Modifier.menuAnchor()
+            modifier = Modifier.menuAnchor(
+                type = MenuAnchorType.PrimaryNotEditable
+            )
         )
 
         ExposedDropdownMenu(
