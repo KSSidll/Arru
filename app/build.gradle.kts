49f3530efb843fe66579e1dade97b750496756fe
Szymon Kolasa <kolasasidll@gmail.com>
2025-05-29 00:19:06 +0200

    Maintenance: Bump version for 2.5.5 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.kssidll.arru"
         minSdk = 21
         targetSdk = 35
-        versionCode = 42
-        versionName = "2.5.4"
+        versionCode = 43
+        versionName = "2.5.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
