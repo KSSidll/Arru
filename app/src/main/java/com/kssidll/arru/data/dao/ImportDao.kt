7406be4455a354e94d9a29c10ca35badb840fc2d
Szymon Kolasa <kolasasidll@gmail.com>
2025-05-30 21:30:31 +0200

    Feat: Delete data before importing

--- app/src/main/java/com/kssidll/arru/data/dao/ImportDao.kt
+++ app/src/main/java/com/kssidll/arru/data/dao/ImportDao.kt
@@ -3,6 +3,7 @@ package com.kssidll.arru.data.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Transaction
 import com.kssidll.arru.data.data.Item
 import com.kssidll.arru.data.data.Product
@@ -18,23 +19,62 @@ interface ImportDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertShops(entities: List<Shop>)
 
+    @Query("DELETE FROM Shop")
+    suspend fun deleteShops()
+
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertProducers(entities: List<ProductProducer>)
+
+    @Query("DELETE FROM ProductProducer")
+    suspend fun deleteProducers()
+
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCategories(entities: List<ProductCategory>)
 
+    @Query("DELETE FROM ProductCategory")
+    suspend fun deleteCategories()
+
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTransactions(entities: List<TransactionBasket>)
+
+    @Query("DELETE FROM TransactionBasket")
+    suspend fun deleteTransactions()
+
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertProducts(entities: List<Product>)
 
+    @Query("DELETE FROM Product")
+    suspend fun deleteProducts()
+
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertVariants(entities: List<ProductVariant>)
 
+    @Query("DELETE FROM ProductVariant")
+    suspend fun deleteVariants()
+
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertItems(entities: List<Item>)
+
+    @Query("DELETE FROM Item")
+    suspend fun deleteItems()
+
+
+    @Transaction
+    suspend fun deleteAll() {
+        deleteItems()
+        deleteVariants()
+        deleteProducts()
+        deleteTransactions()
+        deleteCategories()
+        deleteProducers()
+        deleteShops()
+    }
 
     @Transaction
     suspend fun insertAll(
@@ -46,6 +86,8 @@ interface ImportDao {
         variants: List<ProductVariant>,
         items: List<Item>
     ) {
+        deleteAll()
+
         insertShops(shops)
         insertProducers(producers)
         insertCategories(categories)
